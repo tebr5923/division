770a03c7b8f86d39797a12a846b00ccf9fc0d587
Dmitry Tebryaev <tebr5923@mail.ru>
2020-05-20 23:45:29 +0700

    add ConsoleDivider

---
+++ src/main/java/com/foxminded/divider/ConsoleDivider.java
@@ -0,0 +1,9 @@
+package com.foxminded.divider;
+
+import com.foxminded.Representation;
+
+public class ConsoleDivider implements Divider{
+    public Representation divide(int dividend, int divider) {
+        return null;
+    }
+}
