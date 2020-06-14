052e6f99c0df41240f7a872f811a21245cfcb4b7
Dmitry Tebryaev <tebr5923@mail.ru>
2020-06-14 14:45:17 +0700

    refactor generateStingList remove StringBuilder to String.format

--- src/main/java/com/foxminded/printer/ConsolePrinter.java
+++ src/main/java/com/foxminded/printer/ConsolePrinter.java
@@ -26,53 +26,60 @@ public class ConsolePrinter implements Printer {
         for (Representation representation : representations) {
             i++;
             if (i == 1) {
-                stringList.add(new StringBuilder()
-                        .append(minus)
-                        .append(repeatCharSomeTimes(' ', representation.getPosition()))
-                        .append(representation.getNumber())
-                        .append('|')
-                        .append(storage.getDivider())
-                        .toString());
+                stringList.add(String.format("%s%s%s%s%s",
+                        minus,
+                        repeatCharSomeTimes(' ', representation.getPosition()),
+                        representation.getNumber(),
+                        '|',
+                        storage.getDivider()
+                        )
+                );
             } else if (i == 2) {
                 int timesForSpace = lengthInt(storage.getDividend()) -
                         lengthInt(representation.getNumber()) -
                         representation.getPosition(); //pos of 1st line == 0
                 int timesForMinus = Math.max(lengthInt(storage.getDivider()),
                         lengthInt(storage.getResult()));
-                stringList.add(new StringBuilder()
-                        .append(repeatCharSomeTimes(' ', representation.getPosition() + 1))
-                        .append(representation.getNumber())
-                        .append(repeatCharSomeTimes(' ', timesForSpace))
-                        .append('|')
-                        .append(repeatCharSomeTimes('-', timesForMinus))
-                        .toString());
-                stringList.add(new StringBuilder()
-                        .append(repeatCharSomeTimes(' ', representation.getPosition() + 1))
-                        .append(repeatCharSomeTimes('-', lengthInt(representation.getNumber())))
-                        .append(repeatCharSomeTimes(' ', timesForSpace))
-                        .append('|')
-                        .append(storage.getResult())
-                        .toString());
+                stringList.add(String.format("%s%s%s%s%s",
+                        repeatCharSomeTimes(' ', representation.getPosition() + 1),
+                        representation.getNumber(),
+                        repeatCharSomeTimes(' ', timesForSpace),
+                        '|',
+                        repeatCharSomeTimes('-', timesForMinus)
+                        )
+                );
+                stringList.add(String.format("%s%s%s%s%s",
+                        repeatCharSomeTimes(' ', representation.getPosition() + 1),
+                        repeatCharSomeTimes('-', lengthInt(representation.getNumber())),
+                        repeatCharSomeTimes(' ', timesForSpace),
+                        '|',
+                        storage.getResult()
+                        )
+                );
             } else if (i == representations.size()) {
-                stringList.add(new StringBuilder()
-                        .append(repeatCharSomeTimes(' ', representation.getPosition() + 1))
-                        .append(representation.getNumber())
-                        .toString());
+                stringList.add(String.format("%s%s",
+                        repeatCharSomeTimes(' ', representation.getPosition() + 1),
+                        representation.getNumber()
+                        )
+                );
             } else if (i % 2 != 0) {
-                stringList.add(new StringBuilder()
-                        .append(repeatCharSomeTimes(' ', representation.getPosition()))
-                        .append(minus)
-                        .append(representation.getNumber())
-                        .toString());
+                stringList.add(String.format("%s%s%s",
+                        repeatCharSomeTimes(' ', representation.getPosition()),
+                        minus,
+                        representation.getNumber()
+                        )
+                );
             } else if (i % 2 == 0) {
-                stringList.add(new StringBuilder()
-                        .append(repeatCharSomeTimes(' ', representation.getPosition() + 1))
-                        .append(representation.getNumber())
-                        .toString());
-                stringList.add(new StringBuilder()
-                        .append(repeatCharSomeTimes(' ', representation.getPosition() + 1))
-                        .append(repeatCharSomeTimes('-', lengthInt(representation.getNumber())))
-                        .toString());
+                stringList.add(String.format("%s%s",
+                        repeatCharSomeTimes(' ', representation.getPosition() + 1),
+                        representation.getNumber()
+                        )
+                );
+                stringList.add(String.format("%s%s",
+                        repeatCharSomeTimes(' ', representation.getPosition() + 1),
+                        repeatCharSomeTimes('-', lengthInt(representation.getNumber()))
+                        )
+                );
             }
         }
     }
