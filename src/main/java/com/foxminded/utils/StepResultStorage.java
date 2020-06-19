4537a8dbb84d5d07da025fd4d0efa477cf72fc4e
Dmitry Tebryaev <tebr5923@mail.ru>
2020-06-19 14:19:25 +0700

    refactor StepResultStorage to imutable

--- src/main/java/com/foxminded/utils/StepResultStorage.java
+++ src/main/java/com/foxminded/utils/StepResultStorage.java
@@ -3,8 +3,8 @@ package com.foxminded.utils;
 import com.foxminded.storage.Representation;
 
 public class StepResultStorage {
-    private Representation multiplication;
-    private Representation mod;
+    private final Representation multiplication;
+    private final Representation mod;
 
     public StepResultStorage(Representation multiplication, Representation mod) {
         this.multiplication = multiplication;
@@ -15,15 +15,9 @@ public class StepResultStorage {
         return multiplication;
     }
 
-    public void setMultiplication(Representation multiplication) {
-        this.multiplication = multiplication;
-    }
 
     public Representation getMod() {
         return mod;
     }
 
-    public void setMod(Representation mod) {
-        this.mod = mod;
-    }
 }
