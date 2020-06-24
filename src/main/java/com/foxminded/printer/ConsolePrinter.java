fe3f8c9135e9ae1937ea4070ae9b44ce37e11d2a
Dmitry Tebryaev <tebr5923@mail.ru>
2020-06-24 15:29:21 +0700

    del method generateStingList()

--- src/main/java/com/foxminded/printer/ConsolePrinter.java
+++ src/main/java/com/foxminded/printer/ConsolePrinter.java
@@ -3,76 +3,67 @@ package com.foxminded.printer;
 import com.foxminded.storage.Representation;
 import com.foxminded.storage.Storage;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class ConsolePrinter implements Printer {
     @Override
     public void print(Storage storage) {
-        generateStingList(storage).forEach(System.out::println);
-    }
-
-    private List<String> generateStingList(Storage storage) {
-        List<String> stringList;
         List<Representation> representations = storage.getRepresentations();
         int i = 0;
         char minus = '_';
         char dash = '-';
 
-        stringList = generateHeader(storage);
+        printHeader(storage);
         representations.remove(0);
         representations.remove(0);
         for (Representation representation : representations) {
             i++;
             if (i == representations.size()) {
-                stringList.add(String.format("%s%s",
+                System.out.printf("%s%s%n",
                         repeatCharSomeTimes(' ', representation.getPosition() + 1),
-                        representation.getNumber()));
+                        representation.getNumber());
             } else if (i % 2 != 0) {
-                stringList.add(String.format("%s%s%s",
+                System.out.printf("%s%s%s%n",
                         repeatCharSomeTimes(' ', representation.getPosition()),
                         minus,
-                        representation.getNumber()));
+                        representation.getNumber());
             } else {
-                stringList.add(String.format("%s%s",
+                System.out.printf("%s%s%n",
                         repeatCharSomeTimes(' ', representation.getPosition() + 1),
-                        representation.getNumber()));
-                stringList.add(String.format("%s%s",
+                        representation.getNumber());
+                System.out.printf("%s%s%n",
                         repeatCharSomeTimes(' ', representation.getPosition() + 1),
-                        repeatCharSomeTimes(dash, lengthInt(representation.getNumber()))));
+                        repeatCharSomeTimes(dash, lengthInt(representation.getNumber())));
             }
         }
-        return stringList;
     }
 
-    private List<String> generateHeader(Storage storage) {
+    private void printHeader(Storage storage) {
         List<Representation> representations = storage.getRepresentations();
-        List<String> headerList = new ArrayList<>();
         char minus = '_';
         char dash = '-';
 
-        headerList.add(String.format("%s%s%s|%s",
+        System.out.printf("%s%s%s|%s%n",
                 minus,
                 repeatCharSomeTimes(' ', representations.get(0).getPosition()),
                 representations.get(0).getNumber(),
-                storage.getDivider()));
+                storage.getDivider());
         int spacesAmount = lengthInt(storage.getDividend()) -
                 lengthInt(representations.get(1).getNumber()) -
                 representations.get(1).getPosition();
         int minusAmount = Math.max(lengthInt(storage.getDivider()),
                 lengthInt(storage.getResult()));
-        headerList.add(String.format("%s%s%s|%s",
+        System.out.printf("%s%s%s|%s%n",
                 repeatCharSomeTimes(' ', representations.get(1).getPosition() + 1),
                 representations.get(1).getNumber(),
                 repeatCharSomeTimes(' ', spacesAmount),
-                repeatCharSomeTimes(dash, minusAmount)));
-        headerList.add(String.format("%s%s%s|%s",
+                repeatCharSomeTimes(dash, minusAmount));
+        System.out.printf("%s%s%s|%s%n",
                 repeatCharSomeTimes(' ', representations.get(1).getPosition() + 1),
                 repeatCharSomeTimes(dash, lengthInt(representations.get(1).getNumber())),
                 repeatCharSomeTimes(' ', spacesAmount),
-                storage.getResult()));
-        return headerList;
+                storage.getResult());
     }
 
     private int lengthInt(int integer) {
