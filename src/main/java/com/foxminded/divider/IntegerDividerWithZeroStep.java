2cc46d8066757f1564ac87f8e272f1802e9ce859
Dmitry Tebryaev <tebr5923@mail.ru>
2020-07-05 19:42:17 +0700

    extract variables in nextShortDividend()

--- src/main/java/com/foxminded/divider/IntegerDividerWithZeroStep.java
+++ src/main/java/com/foxminded/divider/IntegerDividerWithZeroStep.java
@@ -4,9 +4,13 @@ import com.foxminded.storage.NumberWithPosition;
 
 public class IntegerDividerWithZeroStep extends IntegerDividerTemplate {
     @Override
-    NumberWithPosition nextShortDividend(int mainDividend, NumberWithPosition reminderWithPosition, int positionInMainDividend, int divider) {
-        return new NumberWithPosition(Integer.parseInt(reminderWithPosition.getNumber() +
-                Integer.toString(mainDividend).
-                        substring(positionInMainDividend, positionInMainDividend + 1)), 0);
+    NumberWithPosition nextShortDividend(int mainDividend,
+                                         NumberWithPosition reminderWithPosition,
+                                         int positionInMainDividend,
+                                         int divider) {
+        String nextDigitFromMainDividend = Integer.toString(mainDividend).
+                substring(positionInMainDividend, positionInMainDividend + 1);
+        String nextShortDividend = reminderWithPosition.getNumber() + nextDigitFromMainDividend;
+        return new NumberWithPosition(Integer.parseInt(nextShortDividend), 0);
     }
 }
